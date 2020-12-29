29221aadaaa0ee83152281b47bdcab6f7ef3e475
Klara <sanchez.clara13@gmail.com>
2020-12-29 13:11:34 +0100

    margin top

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="jumbotron text-center">
-    <h1><strong>Clara Sanchez.</strong> FullStack Developer.</h1>
+  <div class="mt-5">
+    <div class="jumbotron text-center">
+      <h1><strong>Clara Sanchez.</strong> FullStack Developer.</h1>
+    </div>
   </div>
 </template>
