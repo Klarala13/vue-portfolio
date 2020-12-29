24014cb3b0afcb6f7a3245ef3d78f84dd23ad699
Klara <sanchez.clara13@gmail.com>
2020-12-29 13:30:09 +0100

    stack responsive

--- src/components/Skills/Stack.vue
+++ src/components/Skills/Stack.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="container" id="stack">
-    <h1>Stack</h1>
-    <table class="table">
-      <tbody>
-        <tr>
-          <td>HTML5</td>
-          <td>CSS 3</td>
-          <td>Bootstrap</td>
-          <td>SASS</td>
-        </tr>
-        <tr>
-          <td>JS / ES6</td>
-          <td>Vue.js</td>
-          <td>React/Redux</td>
-          <td>WordPress</td>
-        </tr>
-        <tr>
-          <td>Git</td>
-          <td>Express</td>
-          <td>Node JS</td>
-          <td>JWT</td>
-        </tr>
-        <tr>
-          <td>Docker</td>
-          <td>MySQL</td>
-          <td>PostgreSQL</td>
-          <td>Mongo DB</td>
-        </tr>
-      </tbody>
-    </table>
-  </div></template
->
+    <div class="row">
+      <div class="col-12">
+        <h1>Stack</h1>
+        <table class="table">
+          <tbody>
+            <tr>
+              <td>HTML5</td>
+              <td>CSS 3</td>
+              <td>Bootstrap</td>
+              <td>SASS</td>
+            </tr>
+            <tr>
+              <td>JS / ES6</td>
+              <td>Vue.js</td>
+              <td>React/Redux</td>
+              <td>WordPress</td>
+            </tr>
+            <tr>
+              <td>Git</td>
+              <td>Express</td>
+              <td>Node JS</td>
+              <td>JWT</td>
+            </tr>
+            <tr>
+              <td>Docker</td>
+              <td>MySQL</td>
+              <td>PostgreSQL</td>
+              <td>Mongo DB</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
